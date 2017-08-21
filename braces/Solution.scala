9fc6ec65222b97359e1d12d2b69bd5d198995b08
jetonbacaj <bacaj.jeton@gmail.com>
2017-08-21 00:03:14 -0400

    Seriously ... why have I not used pattern matching? What was I thinking?!

--- braces/Solution.scala
+++ braces/Solution.scala
@@ -7,47 +7,34 @@ object Solution {
 
 	def braces(values: Array[String]): Array[String] = {
 		def recurse(firstChar: List[Char], chars: List[Char]): String = {
-			chars match {
-				case h :: t => {
-					//println("\tfirstChar = " + firstChar + " chars = " + chars)
-					if (firstChar == Nil) {
-						return recurse(List(h), t)
-					}
 
-					if (h == brackets._2) {
-						if (firstChar.head != brackets._1) {
-							//println("firstChar.head != brackets._1")
-							"NO"
-						} else if (firstChar.head == brackets._1) {
-							//println("firstChar.head == brackets._1")
-							return recurse(firstChar.tail, t)
-						}
-					} else if (h == parens._2) {
-						if (firstChar.head != parens._1) {
-							//println("firstChar.head != parens._1")
-							"NO"
-						} else if (firstChar.head == parens._1) {
-							//println("firstChar.head == parens._1")
-							return recurse(firstChar.tail, t)
-						}
-					} else if (h == curls._2) {
-						if (firstChar.head != curls._1) {
-							//println("firstChar.head != curls._1")
-							"NO"
-						} else if (firstChar.head == curls._1) {
-							//println("firstChar.head == curls._1")
-							return recurse(firstChar.tail, t)
-						}
-					}
+			(firstChar, chars) match {
+				case (Nil, Nil) => "YES"
+				case (h::t, Nil) => "NO"
+				case (Nil, h::t) => return recurse(List(h), t)
 
-					return recurse(h :: firstChar, t)
-				}
-				case Nil => {
-					//println("\tfirstChar = " + firstChar + " chars = Nil")
-					if (firstChar == Nil) {
-						"YES"
-					} else {
-						"NO"
+				case(firstHead::firstTail, charHead::charTail) => {
+
+					(firstHead, charHead) match{
+						case (brackets._1, brackets._2) => return recurse(firstTail, charTail)
+						case (brackets._1, brackets._1) => return recurse(charHead :: firstChar, charTail)
+						case (brackets._1, parens._1) => return recurse(charHead :: firstChar, charTail)
+						case (brackets._1, curls._1) => return recurse(charHead :: firstChar, charTail)
+						case (brackets._1, _) => "NO"
+
+						case (parens._1, parens._2) => return recurse(firstTail, charTail)
+						case (parens._1, parens._1) => return recurse(charHead :: firstChar, charTail)
+						case (parens._1, brackets._1) => return recurse(charHead :: firstChar, charTail)
+						case (parens._1, curls._1) => return recurse(charHead :: firstChar, charTail)
+						case (parens._1, _) => "NO"
+
+						case (curls._1, curls._2) => return recurse(firstTail, charTail)
+						case (curls._1, curls._1) => return recurse(charHead :: firstChar, charTail)
+						case (curls._1, brackets._1) => return recurse(charHead :: firstChar, charTail)
+						case (curls._1, parens._1) => return recurse(charHead :: firstChar, charTail)
+						case (curls._1, _) => "NO"
+
+						case _ => return recurse(charHead :: firstChar, charTail)
 					}
 				}
 			}
@@ -55,6 +42,12 @@ object Solution {
 
 		import scala.collection.mutable.ListBuffer
 		val response = new ListBuffer[String]
+
+
+
+		for()
+
+
 
 		//println("Input: ")
 		for (x <- values) {
@@ -66,7 +59,7 @@ object Solution {
 				if (stringList.head == brackets._2 || stringList.head == parens._2 || stringList.head == curls._2) {
 					response += "NO"
 				} else {
-					response += recurse(List(stringList.head), stringList.tail)
+					response += recurse(Nil, stringList)
 				}
 			}
 		}
